bb77a2a73537cc38da953e0164b1cc841f09f230
Zebiano <sebas1@hotmail.de>
2021-07-20 20:45:51 +0100

    Add: Events to floaters

--- src/hardware.js
+++ src/hardware.js
@@ -8,10 +8,10 @@ const floater1 = new gpio(wateringSystem.rpiGpioPins.floater1, {
     alert: true
 })
 
-// Fire event when update
+// Set floater1 state when updated
 floater1.on('alert', (level) => {
-    if (level == 0) wateringSystem.floater1 = true
-    else if (level == 1) wateringSystem.floater1 = false
+    if (level == 0) wateringSystem.states.floater1 = true
+    else wateringSystem.states.floater1 = false
 })
 
 // Floater 2
@@ -21,11 +21,23 @@ const floater2 = new gpio(wateringSystem.rpiGpioPins.floater2, {
     alert: true
 })
 
+// Set floater2 state when updated
+floater2.on('alert', (level) => {
+    if (level == 0) wateringSystem.states.floater2 = true
+    else wateringSystem.states.floater2 = false
+})
+
 // Floater 3
 const floater3 = new gpio(wateringSystem.rpiGpioPins.floater3, {
     mode: gpio.INPUT,
     pullUpDown: gpio.PUD_UP,
     alert: true
+})
+
+// Set floater3 state when updated
+floater3.on('alert', (level) => {
+    if (level == 0) wateringSystem.states.floater3 = true
+    else wateringSystem.states.floater3 = false
 })
 
 // Floater 4
@@ -35,11 +47,23 @@ const floater4 = new gpio(wateringSystem.rpiGpioPins.floater4, {
     alert: true
 })
 
+// Set floater4 state when updated
+floater4.on('alert', (level) => {
+    if (level == 0) wateringSystem.states.floater4 = true
+    else wateringSystem.states.floater4 = false
+})
+
 // Floater 5
 const floater5 = new gpio(wateringSystem.rpiGpioPins.floater5, {
     mode: gpio.INPUT,
     pullUpDown: gpio.PUD_UP,
     alert: true
+})
+
+// Set floater5 state when updated
+floater5.on('alert', (level) => {
+    if (level == 0) wateringSystem.states.floater5 = true
+    else wateringSystem.states.floater5 = false
 })
 
 // Set glitch filters (debouncing time) of 10ms
