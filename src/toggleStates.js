e3128add1931f4ce762fad9db50417bc6d8813b0
Zebiano <sebas1@hotmail.de>
2021-08-06 19:37:03 +0100

    Update toggleStates.js

--- src/toggleStates.js
+++ src/toggleStates.js
@@ -43,6 +43,8 @@ exports.allFalse = () => {
  */
 exports.readAllFloaters = () => {
     if (hardware) {
+        console.log('Buenos')
+        console.log(hardware.floater1)
         // console.log(Boolean(hardware.floater1.digitalRead()))
         // exports.floater1(Boolean(hardware.valve1.digitalRead()))
         /* exports.floater1(Boolean(hardware.floater1.digitalRead()))
