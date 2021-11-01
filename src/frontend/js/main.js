d83d3a4839b4a959032ec034a3e0cddd3d5e8cd6
Maarten Busse <77109619+Maarten-JB@users.noreply.github.com>
2021-11-01 15:29:17 +0100

    Merge branch 'master' of https://github.com/Zebiano/WateringSystem

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,60 +1,112 @@
 // Variables
-var socket = io()
+var socket = io();
 
 // Refresh states every second
-setInterval(updateStates, 1000)
+setInterval(updateStates, 1000);
 
 // Update timers
-socket.on('valve1Duration', (duration) => document.getElementById('valve1Duration').value = duration)
-socket.on('valve2Duration', (duration) => document.getElementById('valve2Duration').value = duration)
-socket.on('valve3Duration', (duration) => document.getElementById('valve3Duration').value = duration)
-socket.on('valve4Duration', (duration) => document.getElementById('valve4Duration').value = duration)
+socket.on(
+  "valve1Duration",
+  (duration) => (document.getElementById("valve1Duration").value = duration)
+);
+socket.on(
+  "valve2Duration",
+  (duration) => (document.getElementById("valve2Duration").value = duration)
+);
+socket.on(
+  "valve3Duration",
+  (duration) => (document.getElementById("valve3Duration").value = duration)
+);
+socket.on(
+  "valve4Duration",
+  (duration) => (document.getElementById("valve4Duration").value = duration)
+);
 
 /**
  * Send request to server to get states and update frontend accordingly
  */
 function updateStates() {
-    // Ask for states
-    socket.emit("states", (ws, valve1, valve2, valve3, valve4, tapWater, pumpWaterUp, transferWaterDown) => {
-        // Disable or enable checkboxes
-        document.getElementById('switchValve1').disabled = !valve1.stateAllowed ? true : false
-        // document.getElementById('switchValve2').disabled = !valve2.stateAllowed ? true : false
-        document.getElementById('switchValve3').disabled = !valve3.stateAllowed ? true : false
-        document.getElementById('switchValve4').disabled = !valve4.stateAllowed ? true : false
-        document.getElementById('switchTapWater').disabled = !tapWater.stateAllowed ? true : false
-        document.getElementById('switchPumpWaterUp').disabled = !pumpWaterUp.stateAllowed ? true : false
-        document.getElementById('switchTransferWaterDown').disabled = !transferWaterDown.stateAllowed ? true : false
+  // Ask for states
+  socket.emit(
+    "states",
+    (
+      ws,
+      valve1,
+      valve2,
+      valve3,
+      valve4,
+      tapWater,
+      pumpWaterUp,
+      transferWaterDown
+    ) => {
+      // Disable or enable checkboxes
+      document.getElementById("switchValve1").disabled = !valve1.stateAllowed
+        ? true
+        : false;
+      // document.getElementById('switchValve2').disabled = !valve2.stateAllowed ? true : false
+      document.getElementById("switchValve3").disabled = !valve3.stateAllowed
+        ? true
+        : false;
+      document.getElementById("switchValve4").disabled = !valve4.stateAllowed
+        ? true
+        : false;
+      document.getElementById("switchTapWater").disabled =
+        !tapWater.stateAllowed ? true : false;
+      document.getElementById("switchPumpWaterUp").disabled =
+        !pumpWaterUp.stateAllowed ? true : false;
+      document.getElementById("switchTransferWaterDown").disabled =
+        !transferWaterDown.stateAllowed ? true : false;
 
-        // Status message and manual mode
-        document.getElementById('textStatus').innerHTML = ws.status.msg
-        document.getElementById('switchManual').checked = ws.manual
+      // Status message and manual mode
+      document.getElementById("textStatus").innerHTML = ws.status.msg;
+      document.getElementById("switchManual").checked = ws.manual;
 
-        // Valves
-        document.getElementById('switchValve1').checked = ws.states.valve1
-        document.getElementById('switchValve2').checked = ws.states.valve2
-        document.getElementById('switchValve3').checked = ws.states.valve3
-        document.getElementById('switchValve4').checked = ws.states.valve4
-        document.getElementById('switchTapWater').checked = ws.states.tapWater
-        document.getElementById('switchPumpWaterUp').checked = ws.states.pumpWaterUp
-        document.getElementById('switchTransferWaterDown').checked = ws.states.transferWaterDown
+      if (ws.status.msg.substring(3, 10) == "WARNING") {
+        document.getElementById("textStatus").className = "warning";
+        counter = 0;
+      } else {
+        document.getElementById("textStatus").className = "information";
+        if (counter >= 5) {
+          document.getElementById("textStatus").className = "hide";
+        }
 
-        // Rain
-        document.getElementById('switchRain').checked = ws.states.rain
+        if (statusMemory == ws.status.msg) {
+          counter += 1;
+        } else {
+          counter = 0;
+        }
+      }
+      statusMemory = ws.status.msg;
 
-        // Floaters
-        document.getElementById('switchFloater1').checked = ws.states.floater1
-        document.getElementById('switchFloater2').checked = ws.states.floater2
-        document.getElementById('switchFloater3').checked = ws.states.floater3
-        document.getElementById('switchFloater4').checked = ws.states.floater4
-        document.getElementById('switchFloater5').checked = ws.states.floater5
-    })
+      // Valves
+      document.getElementById("switchValve1").checked = ws.states.valve1;
+      document.getElementById("switchValve2").checked = ws.states.valve2;
+      document.getElementById("switchValve3").checked = ws.states.valve3;
+      document.getElementById("switchValve4").checked = ws.states.valve4;
+      document.getElementById("switchTapWater").checked = ws.states.tapWater;
+      document.getElementById("switchPumpWaterUp").checked =
+        ws.states.pumpWaterUp;
+      document.getElementById("switchTransferWaterDown").checked =
+        ws.states.transferWaterDown;
+
+      // Rain
+      document.getElementById("switchRain").checked = ws.states.rain;
+
+      // Floaters
+      document.getElementById("switchFloater1").checked = ws.states.floater1;
+      document.getElementById("switchFloater2").checked = ws.states.floater2;
+      document.getElementById("switchFloater3").checked = ws.states.floater3;
+      document.getElementById("switchFloater4").checked = ws.states.floater4;
+      document.getElementById("switchFloater5").checked = ws.states.floater5;
+    }
+  );
 }
 
 /**
  * Toggle reboot
  */
 function toggleReboot() {
-    io().emit('reboot')
+  io().emit("reboot");
 }
 
 /**
@@ -62,10 +114,10 @@ function toggleReboot() {
  * @param {boolean} state
  */
 function toggleManual(state) {
-    io().emit('manual', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("manual", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -73,11 +125,11 @@ function toggleManual(state) {
  * @param {boolean} state
  */
 function toggleValve1(state) {
-    const duration = document.getElementById('valve1Duration').value * 1000
-    io().emit('valve1', state, duration, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  const duration = document.getElementById("valve1Duration").value * 1000;
+  io().emit("valve1", state, duration, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -85,11 +137,11 @@ function toggleValve1(state) {
  * @param {boolean} state
  */
 function toggleValve2(state) {
-    const duration = document.getElementById('valve2Duration').value * 1000
-    io().emit('valve2', state, duration, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  const duration = document.getElementById("valve2Duration").value * 1000;
+  io().emit("valve2", state, duration, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -97,11 +149,11 @@ function toggleValve2(state) {
  * @param {boolean} state
  */
 function toggleValve3(state) {
-    const duration = document.getElementById('valve3Duration').value * 1000
-    io().emit('valve3', state, duration, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  const duration = document.getElementById("valve3Duration").value * 1000;
+  io().emit("valve3", state, duration, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -109,11 +161,11 @@ function toggleValve3(state) {
  * @param {boolean} state
  */
 function toggleValve4(state) {
-    const duration = document.getElementById('valve4Duration').value * 1000
-    io().emit('valve4', state, duration, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  const duration = document.getElementById("valve4Duration").value * 1000;
+  io().emit("valve4", state, duration, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -121,10 +173,10 @@ function toggleValve4(state) {
  * @param {boolean} state
  */
 function toggleTapWater(state) {
-    io().emit('tapWater', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("tapWater", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -132,10 +184,10 @@ function toggleTapWater(state) {
  * @param {boolean} state
  */
 function togglePumpWaterUp(state) {
-    io().emit('pumpWaterUp', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("pumpWaterUp", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -143,10 +195,10 @@ function togglePumpWaterUp(state) {
  * @param {boolean} state
  */
 function toggleTransferWaterDown(state) {
-    io().emit('transferWaterDown', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("transferWaterDown", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -154,10 +206,10 @@ function toggleTransferWaterDown(state) {
  * @param {boolean} state
  */
 function toggleRain(state) {
-    io().emit('rain', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("rain", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -165,10 +217,10 @@ function toggleRain(state) {
  * @param {boolean} state
  */
 function toggleFloater1(state) {
-    io().emit('floater1', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("floater1", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -176,10 +228,10 @@ function toggleFloater1(state) {
  * @param {boolean} state
  */
 function toggleFloater2(state) {
-    io().emit('floater2', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("floater2", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -187,10 +239,10 @@ function toggleFloater2(state) {
  * @param {boolean} state
  */
 function toggleFloater3(state) {
-    io().emit('floater3', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("floater3", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -198,10 +250,10 @@ function toggleFloater3(state) {
  * @param {boolean} state
  */
 function toggleFloater4(state) {
-    io().emit('floater4', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("floater4", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
 
 /**
@@ -209,8 +261,53 @@ function toggleFloater4(state) {
  * @param {boolean} state
  */
 function toggleFloater5(state) {
-    io().emit('floater5', state, (res) => {
-        if (!res.stateAllowed) alert(res.msg)
-        updateStates()
-    })
+  io().emit("floater5", state, (res) => {
+    if (!res.stateAllowed) alert(res.msg);
+    updateStates();
+  });
 }
+
+//CSS Swipe-Menu
+var start = null;
+const container = document.querySelector(".swipe-area-box");
+
+container.addEventListener("touchstart", function (event) {
+  if (event.touches.length === 1) {
+    //just one finger touched
+    start = event.touches.item(0).clientX;
+  } else {
+    //a second finger hit the screen
+    start = null;
+  }
+});
+
+const content1 = document.querySelector(".content-1-js");
+const content2 = document.querySelector(".content-2-js");
+
+const point1 = document.querySelector(".indicator-1");
+const point2 = document.querySelector(".indicator-2");
+
+container.addEventListener("touchend", function (event) {
+  var offset = 50; //at least 50px are a swipe
+  if (start) {
+    //the finger left the screen
+    var end = event.changedTouches.item(0).clientX;
+
+    //Left
+    if (end > start + offset) {
+      content1.classList.replace("left", "center");
+      content2.classList.replace("center", "right");
+
+      point1.classList.replace("non-active", "active");
+      point2.classList.replace("active", "non-active");
+    }
+    //Right
+    if (end < start - offset) {
+      content1.classList.replace("center", "left");
+      content2.classList.replace("right", "center");
+
+      point1.classList.replace("active", "non-active");
+      point2.classList.replace("non-active", "active");
+    }
+  }
+});
