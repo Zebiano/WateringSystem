5b4240e7756e05282dfb378cdc41d85d7c0f8c16
Zebiano <sebas1@hotmail.de>
2021-12-12 15:18:59 +0000

    Add: Old frontend to `/old`

--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,10 @@ router.get('/test', (req, res) => {
     res.sendFile(`${__dirname}/frontend/html/test.html`)
 })
 
+// Config (manually set states)
+router.get('/old', (req, res) => {
+    res.sendFile(`${__dirname}/frontend/html/old.html`)
+})
+
 // Exports
 module.exports = router
